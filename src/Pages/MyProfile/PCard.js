da30318979ac5831be68c4a4f5431014b9259e0e
easakibahmad <sakib.sgc.du@gmail.com>
2024-02-03 04:22:50 +0600

    almost done at this time

--- src/Pages/MyProfile/PCard.js
+++ src/Pages/MyProfile/PCard.js
@@ -1,10 +1,14 @@
 import { Button } from 'antd';
 import React from 'react';
+import { FaCheckCircle, FaUserPlus } from 'react-icons/fa';
 
 const PCard = () => {
     return (
       <div className="p-2  w-56 shadow-md ">
-        <div className="h-72 ">
+        <div className="h-72 relative">
+          <p className="absolute  -top-3 left-2 flex items-center justify-start hover:text-blue-600 mt-4 border-blue-600">
+            <FaCheckCircle className="text-green-500" /> 
+          </p>
           <img
             className="h-full w-full"
             src="https://ssl.du.ac.bd/fontView/assets/faculty_image/image_1764.JPG"
@@ -12,8 +16,8 @@ const PCard = () => {
           />
         </div>
         <div>
-          <Button className="hover:text-blue-600 mt-4  border-blue-600">
-            Follow
+          <Button className="flex items-center justify-start hover:text-blue-600 mt-4 border-blue-600">
+            <FaUserPlus className="text-blue-500 mr-2" /> Follow
           </Button>
         </div>
       </div>
