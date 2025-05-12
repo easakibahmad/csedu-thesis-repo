c0458f193c1d1ffe00b4044e109985a38ea67ec2
Sakib <sakib.ahmad@yaana.com>
2025-05-12 23:21:02 +0600

    update firebase config

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA07XgD4NOHbMcnsRhZ44F0b2saNpsZVbk",
-  authDomain: "cseduthesisrepository.firebaseapp.com",
-  projectId: "cseduthesisrepository",
-  storageBucket: "cseduthesisrepository.appspot.com",
-  messagingSenderId: "836675862211",
-  appId: "1:836675862211:web:1b9a0309f257dd82a1ec76",
+  apiKey: "AIzaSyDsxRKJaWeVILQuDsq2MJgRI50hPAj0_uo",
+  authDomain: "csedu-thesis-updated.firebaseapp.com",
+  projectId: "csedu-thesis-updated",
+  storageBucket: "csedu-thesis-updated.firebasestorage.app",
+  messagingSenderId: "593183593788",
+  appId: "1:593183593788:web:412026bf53be73993b7a39"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
